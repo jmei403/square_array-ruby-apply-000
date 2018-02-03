2b2d68fd07c6673e3c21d6b726a6efd5551bc308
Jason Mei <jason.mei403@gmail.com>
2018-02-03 15:02:54 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,5 @@
-# def square_array(array)
-#   squared = []
-#   array.each { |num| squared << num**2 }
-#   squared
-# end
-
 def square_array(array)
-  array.map { |num| num**2 }
+  squared = []
+  array.each { |num| squared << num**2 }
+  squared
 end
